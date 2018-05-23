2123481e0be0283513eeb44dd29d1bd85a201f48
zhihui.wen <huifed@icloud.com>
2018-05-23 11:22:33 +0800

    修改my account页面显示样式

--- src/scripts/common/rule.js
+++ src/scripts/common/rule.js
@@ -3,7 +3,7 @@ define(function(require, exports, module) {
 		emailRule: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/,
 		passwordRule: /^.*(?=.{6,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*? ]).*$/,
 		passwordLengthRule: /^[A-Za-z0-9!@#$%^&*? ]{6,}$/,
-		passwordSpecialRule: /[!@#$%^&*? ]+/,
+		passwordSpecialRule: /[!@#$%^&*?]+/,
 		passwordLetter: /[A-Za-z]+/,
 		passwordNumber: /[0-9]+/,
 	}
